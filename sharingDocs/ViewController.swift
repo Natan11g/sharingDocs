8f4a43c20f2db4bdc16b8fbefc3fe987c938be79
Natan <natan.gluszko@natwest.com>
2022-09-30 14:49:20 +0100

    share a message and link

--- sharingDocs/ViewController.swift
+++ sharingDocs/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnShare(_ sender: Any) {
+        let share: [Any] = ["hahhaha", URL(string: "https://www.youtube.com/watch?v=dQw4w9WgXcQ")!]
+        
+        let activityViewController = UIActivityViewController(activityItems: share, applicationActivities: nil)
+        
+        present(activityViewController, animated: true, completion: nil)
+    }
+    
 }
 
